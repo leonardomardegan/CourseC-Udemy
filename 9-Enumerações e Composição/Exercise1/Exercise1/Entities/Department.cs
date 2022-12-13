1416dbe31ef782f86519d49b0670055863662af3
Leonardo Pires Mardegan <leonardopdrums@hotmail.com>
2022-12-13 16:25:10 -0300

    Exercise 1 cap.9

---
+++ 9-Enumerações e Composição/Exercise1/Exercise1/Entities/Department.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercise1.Entities
+{
+    internal class Department
+    {
+        public string Name { get; set; }
+
+        public Department() { }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}

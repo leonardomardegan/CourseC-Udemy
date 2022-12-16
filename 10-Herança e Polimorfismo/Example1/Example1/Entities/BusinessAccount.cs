e543326fdfd89bf3089b2b67047fdb4f32e43bfb
Leonardo Pires Mardegan <leonardopdrums@hotmail.com>
2022-12-16 09:39:06 -0300

    Exercise 1 cap.10

---
+++ 10-Herança e Polimorfismo/Example1/Example1/Entities/BusinessAccount.cs
@@ -0,0 +1,22 @@
+﻿namespace Example1.Entities
+{
+    internal class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+        
+        public BusinessAccount() { }
+
+        public BusinessAccount(int number, string holder, double balance,double loanLimit) : base(number, holder, balance)
+        {
+            LoanLimit = balance;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}

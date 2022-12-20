81f81a454d2d2ac4e8251980c5e1154d703aa1a8
Leonardo Pires Mardegan <leonardopdrums@hotmail.com>
2022-12-20 13:10:49 -0300

    Exercise cap.11

---
+++ 11-Tratamento de Exceções/Exercise/Exercise/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercise.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+            
+        }
+    }
+}

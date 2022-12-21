ca336bf78cb7f32dbe37586b33b464d56b8fca75
Leonardo Pires Mardegan <leonardopdrums@hotmail.com>
2022-12-21 15:11:30 -0300

    Fixation Exercise cap.11

---
+++ 11-Tratamento de Exceções/FixationExercise/FixationExercise/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,9 @@
+﻿namespace FixationExercise.Entities.Exceptions
+{
+    internal class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}

dd763cf803e15fec57a32728296deabebb029b2f
Leonardo Pires Mardegan <leonardopdrums@hotmail.com>
2023-01-18 11:15:41 -0300

    Exercise 1 Part.1

---
+++ 14-Interfaces/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Solução sem interface
+using Exercise1.Entities;
+using System.Globalization;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rental data");
+            Console.Write("Car model: ");
+            string model = Console.ReadLine();
+            Console.WriteLine("Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.WriteLine("Return (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+        }
+    }
+}

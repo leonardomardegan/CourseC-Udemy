9a85d501bcf4546f3633e3b75b1b35cce4f1e60c
Leonardo Pires Mardegan <leonardopdrums@hotmail.com>
2023-01-23 15:43:36 -0300

    Fixation Exercise cap.14

---
+++ 14-Interfaces/FixationExercise/FixationExercise/Services/PaypalService.cs
@@ -0,0 +1,18 @@
+﻿namespace FixationExercise.Services
+{
+    internal class PaypalService : IOnlinePaymentService
+    {
+        private const double MonthlyInterest = 0.01;
+        private const double FeePercentage = 0.02;
+
+        public double PaymentFee(double amount)
+        {
+            return amount * FeePercentage;
+        }
+
+        public double Interest(double amount, int months)
+        {
+            return amount * MonthlyInterest * months;
+        }
+    }
+}

17d01b1fed6566b255c8e5e4ae238a147869a216
Leonardo Pires Mardegan <leonardopdrums@hotmail.com>
2023-01-24 14:39:42 -0300

    Interface IComparable

---
+++ 14-Interfaces/Example2/Example2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Interface IComparable
+using System.IO;
+using System.Collections.Generic;
+using Example2.Entities;
+
+namespace Example2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\in.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path)) 
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach (Employee emp in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}

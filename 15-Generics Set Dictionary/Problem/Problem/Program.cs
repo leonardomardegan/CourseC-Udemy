752e7f30ce1e782849dcfd3adb35a2fa0cf09fb2
Leonardo Pires Mardegan <leonardopdrums@hotmail.com>
2023-01-25 16:02:12 -0300

    Restrictions of generics

---
+++ 15-Generics Set Dictionary/Problem/Problem/Program.cs
@@ -0,0 +1,34 @@
+﻿// Restrições de generics
+
+using Problem.Entities;
+using Problem.Sevices;
+using System.Globalization;
+
+namespace Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] vect = Console.ReadLine().Split(',');
+                string name = vect[0];
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                list.Add(new Product(name, price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            Product max = calculationService.Max(list);
+
+            Console.WriteLine("Most expensive:");
+            Console.WriteLine(max);
+        }
+    }
+}

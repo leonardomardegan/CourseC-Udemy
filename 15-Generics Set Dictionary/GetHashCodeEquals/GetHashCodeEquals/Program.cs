e913d09874e307c04c34294900cfca520068f2a4
Leonardo Pires Mardegan <leonardopdrums@hotmail.com>
2023-01-26 16:06:33 -0300

    GetHashCode and Equals

---
+++ 15-Generics Set Dictionary/GetHashCodeEquals/GetHashCodeEquals/Program.cs
@@ -0,0 +1,20 @@
+﻿// GetHashCode e Equals personalizados
+
+using GetHashCodeEquals.Entities;
+
+namespace GetHashCodeEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "Alex", Email = "alex@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == b);
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}

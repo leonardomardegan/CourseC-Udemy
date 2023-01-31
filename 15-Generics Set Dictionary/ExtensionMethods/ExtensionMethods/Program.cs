894532294e1c43b05636e9224b229bda3e4f3b24
Leonardo Pires Mardegan <leonardopdrums@hotmail.com>
2023-01-31 15:09:36 -0300

    Extension Methods 1 cap.16

---
+++ 15-Generics Set Dictionary/ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2023, 1, 30, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}

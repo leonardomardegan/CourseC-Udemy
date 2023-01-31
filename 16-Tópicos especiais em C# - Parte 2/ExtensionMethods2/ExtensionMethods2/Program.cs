37c19c772423345ef40618fc3068525e97b70e65
Leonardo Pires Mardegan <leonardopdrums@hotmail.com>
2023-01-31 15:22:21 -0300

    Extension Methods 2

---
+++ 16-Tópicos especiais em C# - Parte 2/ExtensionMethods2/ExtensionMethods2/Program.cs
@@ -0,0 +1,13 @@
+﻿// extension method chamado "cut(int)"
+
+namespace ExtensionMethods2
+{
+    public class ProcessFile
+    {
+        public static void Main()
+        {
+            string s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+}

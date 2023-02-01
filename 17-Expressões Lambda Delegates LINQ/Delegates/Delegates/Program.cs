09d044e483e3d4cd73e910fec6ce6aa38c0fb4e5
Leonardo Pires Mardegan <leonardopdrums@hotmail.com>
2023-02-01 14:10:42 -0300

    Introduction to Delegates

---
+++ 17-Expressões Lambda Delegates LINQ/Delegates/Delegates/Program.cs
@@ -0,0 +1,22 @@
+﻿// Introdução a delegates
+
+using Delegates.Services;
+
+namespace Delegates
+{
+    delegate double BinaryNumericOperation(double n1, double n2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Sum;
+
+            double result = op(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}

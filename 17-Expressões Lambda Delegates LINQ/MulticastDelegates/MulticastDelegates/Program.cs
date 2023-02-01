b7fa9463e3b8f5ffae174d8e0cf365189fe5eb3c
Leonardo Pires Mardegan <leonardopdrums@hotmail.com>
2023-02-01 14:23:22 -0300

    Multicast Delegates

---
+++ 17-Expressões Lambda Delegates LINQ/MulticastDelegates/MulticastDelegates/Program.cs
@@ -0,0 +1,20 @@
+﻿using MulticastDelegates.Services;
+
+namespace MulticastDelegates
+{
+    class Program
+    {
+        delegate void BinaryNumericOperation(double n1, double n2);
+
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+
+            op(a, b);
+        }
+    }
+}
